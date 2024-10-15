ba1c012c1e7f262c3bca322c3d93bb4df2463af3
Mar3kP <152393765+Mar3kP@users.noreply.github.com>
2024-10-15 11:45:41 +0200

    added classes

--- main.py
+++ main.py
@@ -8,3 +8,24 @@ root.iconbitmap("ksionszka.ico")
 
 
 root.mainloop()
+
+
+
+class Ksionszka():
+    def __init__(self, tytul, strony):
+        self.tytul = tytul
+        self.strony = strony
+
+Ksiega1 = Ksionszka("Hobbit", 390)
+
+
+
+class Lista():
+    def __init__(self):
+        self.lista = []
+    def add(self, ksionszka):
+        (self.lista).append(ksionszka)
+        
+
+
+
