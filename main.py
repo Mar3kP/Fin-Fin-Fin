5f5c88f38db4f88438b4a4d7a8f21c2cae660df8
SmileyyONG <przemyslaw.jarosz@infotech.edu.pl>
2024-10-16 13:12:33 +0200

    removed class, we will do one for library and one for tkinter app

--- main.py
+++ main.py
@@ -1,38 +1,25 @@
 import tkinter as tk
 from tkinter import *
 
-# root = tk.Tk()
-# root.geometry("800x500")
-# root.title("Księgarnia")
-# root.iconbitmap("ksionszka.ico")
-
-
-# root.mainloop()
-
-
+root = tk.Tk()
+root.geometry("800x500")
+root.title("Księgarnia")
+root.iconbitmap("ksionszka.ico")
 
 class Ksiegarnia1():
     def __init__(self):
         self.ksiegarnia = []
+
     def add(self, ksionszka):
         self.ksiegarnia.append(ksionszka)
+
     def show(self):
         print(self.ksiegarnia)
+
     def remove(self, miejsce):
         self.ksiegarnia.remove(miejsce)
+
     def edit(self, miejsce, tytul):
         self.ksiegarnia[miejsce] = tytul
 
-
-class Ksiegarnia2():
-    def __init__(self):
-        self.ksiegarnia = []
-    def add(self, ksionszka):
-        self.ksiegarnia.append(ksionszka)
-    def show(self):
-        print(self.ksiegarnia)
-    def remove(self, miejsce):
-        self.ksiegarnia.remove(miejsce)
-    def edit(self, miejsce, tytul):
-        self.ksiegarnia[miejsce] = tytul
-
+root.mainloop()
